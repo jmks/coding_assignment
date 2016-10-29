284308e9f7b340a7a266a64061012ddd48353b48
jmks <jason.schweier@gmail.com>
2016-10-29 12:38:25 -0400

    Second (failing) test
    The examples given will serve as acceptance tests. The second of these forces
    a non-trivial implementation. Since we're accepting a string as input, this
    suggusts to me a two-phase solution: parsing and evaluation.

--- spec/price_estimator_spec.rb
+++ spec/price_estimator_spec.rb
@@ -7,5 +7,9 @@ describe PriceEstimator do
     it "estimates $1,591.58 for job $1,299.99, 3 people, food" do
       expect(subject.estimate("$1,299.99, 3 people, food")).to eq "$1,591.58"
     end
+
+    it "estimates $6,199.81 for job $5,432.00, 1 person, drugs" do
+      expect(subject.estimate("$5,432.00, 1 person, drugs")).to eq "$6,199.81"
+    end
   end
 end
